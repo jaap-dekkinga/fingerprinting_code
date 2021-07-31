46475f619a7039228a16b4d765357665e1068da2
Gerrit Goossen <developer@gerrit.email>
2021-07-30 20:52:41 -0400

    Fingerprints can now be matched even if they are from different lengths of audio. (The larger fingerprint will be truncated to match the smaller one.)

--- Source/Fingerprint.cpp
+++ Source/Fingerprint.cpp
@@ -14,19 +14,14 @@
 
 FingerprintSimilarity CompareFingerprints(const Fingerprint *fingerprint1, const Fingerprint *fingerprint2)
 {
-	if (fingerprint1->dataSize != fingerprint2->dataSize) {
-		FingerprintSimilarity results;
-		results.score = -1.0f;
-		results.similarity = -1.0f;
-		results.mostSimilarFramePosition = 0;
-		results.mostSimilarStartTime = 0.0f;
-		return results;
-	}
+	// select the smaller fingerprint size
+	size_t dataSize = (fingerprint1->dataSize > fingerprint2->dataSize) ? fingerprint2->dataSize : fingerprint1->dataSize;
 
-	vector<uint8_t> data1(fingerprint1->dataSize);
-	memcpy(data1.data(), fingerprint1->data, fingerprint1->dataSize);
-	vector<uint8_t> data2(fingerprint2->dataSize);
-	memcpy(data2.data(), fingerprint2->data, fingerprint2->dataSize);
+	// copy the fingerprint data
+	vector<uint8_t> data1(dataSize);
+	memcpy(data1.data(), fingerprint1->data, dataSize);
+	vector<uint8_t> data2(dataSize);
+	memcpy(data2.data(), fingerprint2->data, dataSize);
 
 	FingerprintSimilarityComputer computer(data1, data2);
 	return computer.getMatchResults();
